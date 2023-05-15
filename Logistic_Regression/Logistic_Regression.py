6062c7aefcda234f52527212f5ad01aea1cb058a
Xingsheng Xu <xuxingsheng163@gmail.com>
2023-05-15 15:31:49 -0700

    Version 0.5

--- Logistic_Regression/Logistic_Regression.py
+++ Logistic_Regression/Logistic_Regression.py
@@ -5,7 +5,7 @@ Logistic Regression for classificating marketing target.
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from matplotlib.animation import FuncAnimation, PillowWriter
+from sklearn.preprocessing import StandardScaler
 
 # Load Data
 market_data = pd.read_csv(
@@ -13,11 +13,12 @@ market_data = pd.read_csv(
 
 # Settings
 theta = np.zeros(4)
-data_size = 400
+data_size = len(market_data)
 iteration = 0
 cost = np.inf
 learning_rate = 0.001
-tolerence = 0.5
+tolerence = 0.1
+max_iterations = 1000
 cost_memo = []
 
 
@@ -36,12 +37,13 @@ def cost_func(theta, X, Y):
 training_set_X = market_data.iloc[:, 1:4]
 training_set_Y = market_data.iloc[:, 4]
 
-# Change Unit in 'EstimatedSalary' Column as $k
-training_set_X['EstimatedSalary'] /= 1000
-
 # Change 'Gender' Column as 'Male':1 and 'Female':0
 training_set_X['Gender'] = training_set_X['Gender'].replace(
     {'Male': 1, 'Female': 0})
+
+# Standardize the features
+scaler = StandardScaler()
+training_set_X = scaler.fit_transform(training_set_X)
 
 # Add A Column of Ones to The Training Set As Intercept Term
 X = np.hstack((np.ones((data_size, 1)), training_set_X)).T
@@ -49,7 +51,7 @@ X = np.hstack((np.ones((data_size, 1)), training_set_X)).T
 # Impletement Logistic Regression Training
 
 # Loop through the entire dataset for each epoch
-while cost >= tolerence:
+while cost >= tolerence and iteration <= max_iterations:
     for i in range(data_size):
         X_sample = X[:, i]
         y_sample = training_set_Y[i]
@@ -65,8 +67,18 @@ print(f"Training finished after {iteration} iterations.")
 print(f"Theta values:{theta}")
 
 # Plot cost vs. iteration
-plt.plot(range(1, iteration + 1), cost_memo, 'o')
+plt.plot(range(1, iteration + 1), cost_memo)
 plt.xlabel('Iteration')
 plt.ylabel('Cost')
 plt.title('Cost vs. Iteration')
 plt.show()
+
+
+# Predict the purchased output by trained model
+X_test_raw = np.array([1, 49, 28000])
+X_test_std = scaler.transform(X_test_raw.reshape(1, -1))
+X_test = np.hstack([1, X_test_std[0]])
+
+Y_test = h_func(theta, X_test)
+print(
+    f'The customer will {"purchase" if Y_test.round() else "not purchase"} the product.')
