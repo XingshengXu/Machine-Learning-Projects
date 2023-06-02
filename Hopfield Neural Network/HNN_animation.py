8711aed4c9c305a5fa93fe87fccd78428a09cad4
Xingsheng Xu <xuxingsheng163@gmail.com>
2023-06-02 16:16:05 -0700

    Version 0.5

---
+++ Hopfield Neural Network/HNN_animation.py
@@ -0,0 +1,115 @@
+# Hopfield Neural Network for Pattern Association
+import idx2numpy
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.animation import FuncAnimation
+
+
+def sign_func(x):
+    """ Sign Function"""
+    if x > 0:
+        return 1
+    elif x < 0:
+        return -1
+    else:
+        return x
+
+
+def flipping_noise(image, flip_fraction):
+    """
+    This function takes an image and a fraction representing the amount of noise
+    to add and returns a noisy image. The noise is simply flipping of pixels.
+    """
+    # Flatten the image if it is not
+    if len(image.shape) > 1:
+        image = image.flatten()
+
+    # Generate a mask for flipping pixels
+    noise_mask = np.random.choice([False, True], len(
+        image), p=[1-flip_fraction, flip_fraction])
+
+    # Return a copy of the image with the pixels at the mask indices flipped
+    return np.where(noise_mask, -image, image)
+
+
+# Load training and testing data sets
+try:
+    train_set = idx2numpy.convert_from_file(
+        'Hopfield Neural Network/dataset/train-images.idx3-ubyte')
+    label_set = idx2numpy.convert_from_file(
+        'Hopfield Neural Network/dataset/train-labels.idx1-ubyte')
+except FileNotFoundError as e:
+    print("One or more data files not found.")
+    print(e)
+    exit()
+
+# Parameters
+threshold = 128
+pattern_size = 2
+pattern_index = 1
+is_equal = False
+train_index = []
+
+# Pick training images
+for digit in range(pattern_size):
+    digit_indices = np.where(label_set == digit)[0]
+    train_index.append(digit_indices[0])
+
+train_images = train_set[train_index]
+
+# Transpose the matrix so that the image indices are the third dimension
+X_train = np.transpose(train_images, (1, 2, 0))
+
+# Reshape the images (28*28) to intput data (784*1)
+X_train = np.reshape(X_train, (784, pattern_size))
+
+# Transfer greyscale images to bipolar images
+X_train = np.where(X_train > threshold, 1, -1)
+
+# Hopfield neural network training
+weights = X_train @ X_train.T
+
+# Keep the diagonal elements to be zero
+np.fill_diagonal(weights, 0)
+
+# Hopfield neural network noise reduction
+Y_test_prev = flipping_noise(X_train[:, pattern_index], 0.1)
+
+# Initialize previous and current states
+Y_test_curr = np.copy(Y_test_prev)
+
+# Vectorize the sign function and apply it to network
+sign_func_vec = np.vectorize(sign_func)
+
+# Create a figure for the animation
+fig, ax = plt.subplots()
+
+# Initialize the neuron index
+i_neuron = 0
+
+# The function to update the figure for each frame of the animation
+
+
+def update(frame):
+    global Y_test_prev, Y_test_curr, is_equal, i_neuron
+    ax.clear()
+
+    # Update the next neuron
+    i_neuron = frame % len(Y_test_curr)
+    Y_test_curr[i_neuron] = sign_func_vec(weights[i_neuron] @ Y_test_prev)
+    Y_test_prev = np.copy(Y_test_curr)
+
+    # Plot the current state
+    output_image = Y_test_curr.reshape(28, 28)
+    ax.imshow(output_image, cmap='gray')
+    ax.axis('off')
+
+
+# Determine the total number of frames (one per neuron update)
+n_frames = len(Y_test_curr) * 10  # For example, 10 full cycles
+
+# Create the animation
+ani = FuncAnimation(fig, update, frames=range(
+    n_frames), interval=1, repeat=False)
+
+plt.show()
