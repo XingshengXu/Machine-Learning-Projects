02a87e36cac0842e53b1ebc2a1fddef4d8f8bdc0
Xingsheng Xu <xuxingsheng163@gmail.com>
2023-07-07 14:29:07 -0700

    Version 0.5

--- Decision Tree/DT.py
+++ Decision Tree/DT.py
@@ -1,11 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
-from sklearn.datasets import make_blobs
 from sklearn.metrics import classification_report, confusion_matrix, mean_squared_error
 
 
 class Node():
+    """ Represents a single node in the Decision Tree.
+    
+    Args:
+        feature_threshold (tuple): Stores the feature index and threshold value used for
+                                   the decision at the node. Format: (feature index, threshold).
+        class_frequency (numpy.ndarray): Frequency of classes at this node.
+        prediction (float): The predicted class for the samples at this node in the case of 
+                            classification or the predicted target value in the case of regression.
+    
+    Attributes:
+        left (Node): The left child node of the current node.
+        right (Node): The right child node of the current node.
+    """
+
     def __init__(self, feature_threshold, class_frequency=None, prediction=None):
         self.feature_threshold = feature_threshold
 
@@ -21,6 +34,18 @@ class Node():
 
 
 class Tree():
+    """
+    The super Tree class for the ClassificationTree class and the RegressionTree class.
+
+    Args:
+        max_depth (int, optional): Maximum depth for the tree.
+        min_split_samples (int, optional): Minimum number of samples required to split a node.
+        min_leaf_samples (int, optional): Minimum number of samples required to be at a leaf node.
+        
+    Attributes:
+        root (Node): The root node of the Decision Tree.
+    """
+
     def __init__(self, max_depth=np.inf, min_split_samples=2, min_leaf_samples=1):
         self.root = None
 
@@ -30,15 +55,15 @@ class Tree():
             raise AttributeError(
                 'Invalid max_depth value, max_depth must be greater than zero.')
 
-        if min_leaf_samples > 0 and min_split_samples > 2 * min_leaf_samples:
+        if min_leaf_samples > 0 and min_split_samples >= 2 * min_leaf_samples:
             self.min_leaf_samples = min_leaf_samples
             self.min_split_samples = min_split_samples
         else:
             raise AttributeError(
                 'Invalid values: min_samples_leaf must be greater than zero, and min_samples_split must be no less than twice min_samples_leaf.')
 
-    def split(self, split_threshold, split_column, X, y):
-        '''Split the dataset based on the given feature and threshold value.'''
+    def split(self, split_column, split_threshold, X, y):
+        """Split the dataset based on the given feature and threshold value."""
 
         left_mask = X[:, split_column] <= split_threshold
         right_mask = X[:, split_column] > split_threshold
@@ -53,7 +78,7 @@ class Tree():
         return left, y_left, right, y_right
 
     def __predict_probs(self, subtree, sample):
-        '''Private function to return the class frequencies of the sample associated with the node.'''
+        """Private method to return the class frequencies of the sample associated with the node."""
 
         # Reshape the input if the sample is a scalar
         if sample.ndim == 0:
@@ -71,7 +96,7 @@ class Tree():
         return self.__predict_probs(subtree.right, sample)
 
     def predict_probs(self, sample):
-        '''Predict the class probabilities of the sample for regression.'''
+        """Predict the class probabilities of the sample for regression."""
 
         if self.root is None:
             raise AttributeError(
@@ -83,7 +108,7 @@ class Tree():
         return classes
 
     def predict_class(self, sample):
-        '''Predict the class of the sample based on frequencies for classification.'''
+        """Predict the class of the sample based on frequencies for Classification Tree."""
 
         if self.root is None:
             raise AttributeError(
@@ -93,12 +118,40 @@ class Tree():
         classes = self.__predict_probs(self.root, sample)
         return np.argmax(classes)
 
+    def predict_test(self, test_Y, pred_Y):
+        """This function prints the classification report and plots the confusion 
+        matrix for the given actual and predicted labels."""
+
+        # Print classification report
+        print(classification_report(test_Y, pred_Y, zero_division=0))
+
+        # Create confusion matrix
+        cm = confusion_matrix(test_Y, pred_Y)
+        sns.heatmap(cm, annot=True, fmt='d')
+        plt.title('Confusion Matrix')
+        plt.xlabel('Predicted')
+        plt.ylabel('Actual')
+        plt.show()
+
 
 class ClassificationTree(Tree):
+    """ A Classification Tree is a type of Decision Tree that involves top-down, greedy, and the 
+    recursive partitioning of the data space to create a tree-like model for classification tasks.
+    
+    Args:
+        criterion (str, optional): The metric to determine the efficiency of a split decision.
+        max_depth (int, optional): Maximum depth for the tree.
+        min_split_samples (int, optional): Minimum number of samples required to split a node.
+        min_leaf_samples (int, optional): Minimum number of samples required to be at a leaf node.
+        
+    Attributes:
+        root (Node): The root node of Classification Tree.
+    """
+
     def __init__(self, criterion='gini', max_depth=np.inf, min_split_samples=2, min_leaf_samples=1):
 
         super().__init__(max_depth, min_split_samples, min_leaf_samples)
-
+        
         if criterion == 'gini' or criterion == 'entropy':
             self.criterion = criterion
         else:
@@ -106,7 +159,7 @@ class ClassificationTree(Tree):
                 f"Invalid criterion, 'gini' and 'entropy' are available.")
 
     def gini_impurity(self, y):
-        '''Calculate the Gini impurity of the specified node.'''
+        """Calculate the Gini impurity of the specified node."""
 
         _, counts = np.unique(y, return_counts=True)
         total = np.sum(counts)
@@ -116,7 +169,7 @@ class ClassificationTree(Tree):
         return gini
 
     def entropy(self, y):
-        '''Calculate the Entropy of the specified node.'''
+        """Calculate the Entropy of the specified node."""
 
         _, counts = np.unique(y, return_counts=True)
         total = np.sum(counts)
@@ -126,7 +179,7 @@ class ClassificationTree(Tree):
         return entropy
 
     def information_gain(self, y_left, y_right, y_parent):
-        '''Calculate the information gain of the specified split.'''
+        """Calculate the information gain of the specified split."""
 
         n = y_parent.shape[0]
         entropy_parent = self.entropy(y_parent)
@@ -139,8 +192,8 @@ class ClassificationTree(Tree):
         return info_gain
 
     def calculate_improvement(self, y_left, y_right, y_parent):
-        '''Calculate the appropriate impurity value based on the criterion
-        being used in the model.'''
+        """Calculate the appropriate impurity value based on the criterion
+        being used in the model."""
 
         n = y_parent.shape[0]
         if self.criterion == 'gini':
@@ -156,7 +209,8 @@ class ClassificationTree(Tree):
             return info_gain
 
     def get_split_points(self, X):
-        '''Calculates the split points in the dataset. Returns the list of these split points and the corresponding feature indices.'''
+        """Calculates the split points in the dataset. Returns the list 
+        of these split points and the corresponding feature indices."""
 
         feature_indices = []
         split_points = []
@@ -168,14 +222,14 @@ class ClassificationTree(Tree):
                 split = (x[j] + x[j+1])/2.0
                 split_points.append(split)
                 feature_indices.append(i)
-        return split_points, feature_indices
+        return feature_indices, split_points
 
     def best_split(self, X, y):
-        '''Determine the best split for a decision tree based on the specified impurity criterion.
-        The function will return the split that results in the largest decrease in impurity.'''
+        """Determine the best split for a Classification Tree based on the specified impurity criterion.
+        The function will return the split that results in the largest decrease in impurity."""
 
         # Get possible split points
-        split_points, feature_indices = self.get_split_points(X)
+        feature_indices, split_points = self.get_split_points(X)
 
         # Initialize best impurity measure
 
@@ -184,20 +238,54 @@ class ClassificationTree(Tree):
         else:
             best_impurity = -1
 
-        # Evaluate each possible split point
-        for feature_index, split_value in zip(feature_indices, split_points):
+        # Evaluate the impurity of each possible split point
+        for feature_index, split_point in zip(feature_indices, split_points):
+            left, y_left, right, y_right = self.split(
+                feature_index, split_point, X, y)
+            current_impurity = self.calculate_improvement(y_left, y_right, y)
 
-            lower, y_lower, upper, y_upper = self.split(
-                split_value, feature_index, X, y)
-            current_impurity = self.calculate_improvement(y_lower, y_upper, y)
-
-            # Update if this split is better than the best so far
+            # Update the best split that results in the largest decrease in impurity
             is_better_gini_split = self.criterion == 'gini' and current_impurity < best_impurity
             is_better_entropy_split = self.criterion == 'entropy' and current_impurity > best_impurity
+
             if is_better_gini_split or is_better_entropy_split:
-                best_left = np.c_[lower, y_lower]
-                best_right = np.c_[upper, y_upper]
+                best_left = np.c_[left, y_left]
+                best_right = np.c_[right, y_right]
                 best_impurity = current_impurity
-                best_split_point = (split_value, feature_index)
+                best_split_point = (feature_index, split_point)
 
         return best_left, best_right, best_split_point
+
+    def __fit(self, subtree, curr_depth, X, y):
+        """Private method to recursively build the Classification Tree until all instances have been correctly classified 
+        or certain criteria have been satisfied."""
+
+        class_frequency = np.bincount(y.astype(int))
+
+        is_gini_zero = (
+            self.criterion == 'gini' and self.gini_impurity(y) == 0)
+        is_entropy_zero = (
+            self.criterion == 'entropy' and self.entropy(y) == 0)
+        if (
+            curr_depth > self.max_depth or
+            len(X) < self.min_split_samples or
+            is_gini_zero or is_entropy_zero
+        ):
+            return Node(None, class_frequency)
+        else:
+            (best_left, best_right, best_split_point) = self.best_split(X, y)
+            if len(best_left) < self.min_leaf_samples or len(best_right) < self.min_leaf_samples:
+                return Node(None, class_frequency)
+            else:
+                subtree = Node(best_split_point, class_frequency)
+                X_left, y_left = best_left[:, :-1], best_left[:, -1]
+                X_right, y_right = best_right[:, :-1], best_right[:, -1]
+                subtree.left = self.__fit(
+                    subtree.left, curr_depth + 1, X_left, y_left)
+                subtree.right = self.__fit(
+                    subtree.right, curr_depth + 1, X_right, y_right)
+                return subtree
+
+    def fit(self, X, y):
+        '''Build the Classification Tree by recursively calling the __fit method.'''
+        self.root = self.__fit(self.root, 1, X, y)
