5b98555acb2e2ffbf1ce0d4826977d91267b9d1d
Xingsheng Xu <xuxingsheng163@gmail.com>
2023-07-11 16:31:46 -0700

    Complete Version 1.0

--- Decision Tree/decision_tree_utils.py
+++ Decision Tree/decision_tree_utils.py
@@ -3,25 +3,39 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 from matplotlib.widgets import Button
-from sklearn.metrics import classification_report, confusion_matrix, mean_squared_error
-from decisiontree import ClassificationTree
+from sklearn.metrics import classification_report, confusion_matrix, r2_score
+from decisiontree import ClassificationTree, RegressionTree
 
 
-def evaluate_model(test_Y, pred_Y):
-    """This function prints the classification report and plots the confusion 
-    matrix for the given actual and predicted labels."""
+def evaluate_model(test_y, pred_y, model_type):
+    """Evaluates the performance of a model and prints the appropriate performance metrics 
+    based on the type of the model. For classification models, it prints the classification 
+    report and plots the confusion matrix. For regression models, it calculates and prints 
+    the R-squared score."""
 
-    # Print classification report
-    print(classification_report(test_Y, pred_Y, zero_division=0))
+    if model_type == 'regression':
 
-    # Create confusion matrix
-    cm = confusion_matrix(test_Y, pred_Y)
-    sns.heatmap(cm, annot=True, fmt='d')
+        # Calculate R-squared
+        r2 = r2_score(test_y, pred_y)
 
-    plt.title('Confusion Matrix')
-    plt.xlabel('Predicted')
-    plt.ylabel('Actual')
-    plt.show()
+        print("R-squared score (R²): ", r2)
+
+    elif model_type == 'classification':
+
+        # Print classification report
+        print(classification_report(test_y, pred_y, zero_division=0))
+
+        # Create confusion matrix
+        cm = confusion_matrix(test_y, pred_y)
+        sns.heatmap(cm, annot=True, fmt='d')
+
+        plt.title('Confusion Matrix')
+        plt.xlabel('Predicted')
+        plt.ylabel('Actual')
+        plt.show()
+
+    else:
+        print("Invalid model type. Please choose either 'regression' or 'classification'.")
 
 
 def create_contour_plot(tree, X, y, resolution=500, alpha=0.5):
@@ -65,13 +79,36 @@ def create_contour_plot(tree, X, y, resolution=500, alpha=0.5):
     plt.show()
 
 
+def create_regression_plot(tree, X, y):
+    """Create a plot to visualize the Regression Tree predictions."""
+
+    X = np.array(X)
+    y = np.array(y)
+
+    # Get the predictions of Regression Tree
+    pred_y = [tree.predict_value(x) for x in X]
+
+    # Plot the predictions of each model on the same set of axes.
+    plt.figure(figsize=(10, 7))
+
+    plt.scatter(X, y, s=20, c='black', marker='x', label='Target')
+    plt.plot(sorted(X), [pred for _, pred in sorted(
+        zip(X, pred_y))], c='red', label='Regression')
+
+    plt.legend(loc='upper center')
+    plt.xlabel('Feature one (Dimension one)')
+    plt.ylabel('Feature two (Dimension two)')
+    plt.title('Regression Tree Visualization Demo')
+    plt.show()
+
+
 def interactive_data_collection_classification():
     """
     Create an interactive plot for collecting data points for a Classification Task.
 
     The function allows you to interactively add points of two classes by left-clicking
-    for class 0 and right-clicking for class 1. It also allows you to train a classification 
-    tree and visualize the decision boundaries by clicking the 'Train' button, or to clear 
+    for class 0 and right-clicking for class 1. It also allows you to train a Classification 
+    Tree and visualize the decision boundaries by clicking the 'Train' button, or to clear 
     all data points and start over by clicking the 'Clean' button.
     """
 
@@ -131,6 +168,75 @@ def interactive_data_collection_classification():
 
     ax_button_clear = plt.axes([0.55, 0.01, 0.2, 0.06])
     button_clear = Button(ax_button_clear, 'Clean')
+    button_clear.on_clicked(onclean)
+
+    # Register onclick function
+    fig.canvas.mpl_connect('button_press_event', onclick)
+
+    plt.show()
+
+
+def interactive_data_collection_regression():
+    """
+    Create an interactive plot for collecting data points for a Regression Task.
+
+    The function allows you to interactively add sample points by left-clicking. 
+    It also allows you to train a Regression Tree and visualize the regression 
+    plots by clicking the 'Train' button, or to clear all data points and start 
+    over by clicking the 'Clear' button.
+    """
+
+    # Initialize click coordinates and values
+    data_points = []
+
+    # Create an interactive plot
+    fig, ax = plt.subplots(figsize=(10, 7))
+
+    # Set plot properties
+    ax.set_xlim([-5, 5])
+    ax.set_ylim([-50, 50])
+    ax.set_title("$\mathbf{Regression\ Tree\ Demo\ on\ Manually\ Generated\ Data}$"
+                 "\nLeft click to input data.")
+
+    # Define onclick function for collecting data
+    def onclick(event):
+        if event.inaxes == ax:
+            data_points.append((event.xdata, event.ydata))
+            ax.scatter(event.xdata, event.ydata, s=20, c='black', marker='x')
+            fig.canvas.draw()
+
+    # Define onpress function for training model
+    def onpress(event):
+        if len(data_points) > 1:
+            # reshaping to meet the input requirement
+            X = np.array([dp[0] for dp in data_points])
+            y = np.array([dp[1] for dp in data_points])
+            tree = RegressionTree(criterion='mse', max_depth=3)
+            tree.fit(X, y)
+            create_regression_plot(tree, X, y)
+
+    # Define onclean function for resetting data
+    def onclean(event):
+        data_points.clear()
+
+        # Remove drawn elements
+        for coll in (ax.collections + ax.lines):
+            coll.remove()
+        fig.canvas.draw()
+
+        ax.set_xlim([-5, 5])
+        ax.set_ylim([-50, 50])
+        ax.set_title("$\mathbf{Regression\ Tree\ Demo\ on\ Manually\ Generated\ Data}$"
+                     "\nLeft click to input data.")
+        fig.canvas.draw()
+
+    # Create 'Train' and 'Clear' buttons
+    ax_button_train = plt.axes([0.25, 0.01, 0.2, 0.06])
+    button_train = Button(ax_button_train, 'Train')
+    button_train.on_clicked(onpress)
+
+    ax_button_clear = plt.axes([0.55, 0.01, 0.2, 0.06])
+    button_clear = Button(ax_button_clear, 'Clear')
     button_clear.on_clicked(onclean)
 
     # Register onclick function
--- Decision Tree/decisiontree.py
+++ Decision Tree/decisiontree.py
@@ -95,7 +95,7 @@ class Tree():
         # Otherwise, predict on the right subtree
         return self.__predict_probs(subtree.right, sample)
 
-    def predict_probs(self, sample):
+    def predict_value(self, sample):
         """Predict the class probabilities of the sample for regression."""
 
         if self.root is None:
@@ -103,9 +103,8 @@ class Tree():
                 f"Model not fitted, call 'fit' with appropriate arguments before using model.")
 
         # Perform prediction traversing the entire tree
-        classes = self.__predict_probs(self.root, sample)
-        classes /= np.sum(classes)
-        return classes
+        value = self.__predict_probs(self.root, sample)
+        return value
 
     def predict_class(self, sample):
         """Predict the class of the sample based on frequencies for Classification Tree."""
@@ -120,7 +119,7 @@ class Tree():
 
 
 class ClassificationTree(Tree):
-    """ A Classification Tree is a type of Decision Tree that involves top-down, greedy, and the 
+    """ A Classification Tree is a type of Decision Tree that involves top-down, greedy, and 
     recursive partitioning of the data space to create a tree-like model for classification tasks.
 
     Args:
@@ -142,7 +141,7 @@ class ClassificationTree(Tree):
             self.criterion = criterion
         else:
             raise AttributeError(
-                f"Invalid criterion, 'gini' and 'entropy' are available.")
+                "Invalid criterion, 'gini' and 'entropy' are available.")
 
     def gini_impurity(self, y):
         """Calculate the Gini impurity of the specified node."""
@@ -182,6 +181,7 @@ class ClassificationTree(Tree):
         being used in the model."""
 
         n = y_parent.shape[0]
+
         if self.criterion == 'gini':
             gini_left = self.gini_impurity(y_left)
             gini_right = self.gini_impurity(y_right)
@@ -249,6 +249,7 @@ class ClassificationTree(Tree):
         """Private method to recursively build the Classification Tree until all instances have been 
         correctly classified or certain criteria have been satisfied."""
 
+        # Count the frequency of each class in node
         class_frequency = np.bincount(y.astype(int))
 
         # Base case for recursion: If any of the regulation criteria are met, return a leaf node
@@ -256,6 +257,7 @@ class ClassificationTree(Tree):
             self.criterion == 'gini' and self.gini_impurity(y) == 0)
         is_entropy_zero = (
             self.criterion == 'entropy' and self.entropy(y) == 0)
+
         if (
             curr_depth > self.max_depth or
             len(X) < self.min_split_samples or
@@ -286,4 +288,148 @@ class ClassificationTree(Tree):
 
     def fit(self, X, y):
         '''Build the Classification Tree by recursively calling the __fit method.'''
+        self.root = self.__fit(self.root, 1, X, y)
+
+
+class RegressionTree(Tree):
+    """ A Regression Tree is a type of Decision Tree that involves top-down, greedy, and 
+    recursive partitioning of the data space to create a tree-like model for regression tasks.
+
+    Args:
+        criterion (str, optional): The metric to determine the efficiency of a split decision.
+        max_depth (int, optional): Maximum depth for the tree.
+        min_split_samples (int, optional): Minimum number of samples required to split a node.
+        min_leaf_samples (int, optional): Minimum number of samples required to be at a leaf node.
+
+    Attributes:
+        root (Node): The root node of Regression Tree.
+    """
+
+    def __init__(self, criterion='mse', max_depth=np.inf, min_split_samples=2, min_leaf_samples=1):
+        super().__init__(max_depth, min_split_samples, min_leaf_samples)
+
+        # Check if the criterion is valid
+        if criterion == 'mse' or criterion == 'mae':
+            self.criterion = criterion
+        else:
+            raise AttributeError(
+                "Invalid criterion, 'mse' and 'mae' are available.")
+
+    def MSE(self, y):
+        '''Calculate the mean squared error of the samples at the node per feature.'''
+        y_hat = np.mean(y)
+        mse = np.sum((y_hat - y)**2)
+        return mse
+
+    def MAE(self, y):
+        """Calculate the mean absolute error of the samples at the node per feature."""
+        y_hat = np.mean(y)
+        mae = np.sum(np.abs(y_hat - y))
+        return mae
+
+    def calculate_improvement(self, y_left, y_right, y_parent):
+        """Calculate the reduction in error based on the criterion
+        being used in the model."""
+
+        n = y_parent.shape[0]
+
+        if self.criterion == 'mse':
+            #! MSE_Total = Σ ( |Di| / |D| ) * MSE(Di)
+            total_mse = (y_left.shape[0] / n) * self.MSE(y_left) + (
+                y_right.shape[0] / n) * self.MSE(y_right)
+            return total_mse
+        else:
+            #! MAE_Total = Σ ( |Di| / |D| ) * MAE(Di)
+            total_mae = (y_left.shape[0] / n) * self.MAE(y_left) + (
+                y_right.shape[0] / n) * self.MAE(y_right)
+            return total_mae
+
+    def get_split_points(self, X):
+        """Calculates the split points in the dataset. Returns the list 
+        of these split points and the corresponding feature indices."""
+
+        feature_indices = []
+        split_points = []
+
+        # Calculates the split points by averaging each pair of adjacent unique values in each feature.
+        for i, x in enumerate(np.sort(X.T)):
+            x = np.unique(x)
+            for j in range(x.shape[0]-1):
+                split = (x[j] + x[j+1])/2.0
+                split_points.append(split)
+                feature_indices.append(i)
+        return feature_indices, split_points
+
+    def best_split(self, X, y):
+        """Determine the best split for a Regression Tree based on the specified error criterion.
+        The function will return the split that results in the largest decrease in error."""
+
+        if X.ndim == 1:
+            X = X.reshape((-1, 1))
+
+        # Get possible split points
+        feature_indices, split_points = self.get_split_points(X)
+
+        # Initialize lowest error measure
+        lowest_error = np.inf
+
+        # Initialize best left, right, and split_point as None
+        best_left, best_right, best_split_point = None, None, None
+
+        # Evaluate the error of each possible split point
+        for feature_index, split_point in zip(feature_indices, split_points):
+            left, y_left, right, y_right = self.split(
+                feature_index, split_point, X, y)
+            current_error = self.calculate_improvement(y_left, y_right, y)
+
+            # Update the best split if current error is the lowest one
+            if current_error < lowest_error:
+                best_left = np.c_[left, y_left]
+                best_right = np.c_[right, y_right]
+                lowest_error = current_error
+                best_split_point = (feature_index, split_point)
+
+        return best_left, best_right, best_split_point
+
+    def __fit(self, subtree, curr_depth, X, y):
+        """Private method to recursively build the Regression Tree until all instances have been 
+        correctly regressed or certain criteria have been satisfied."""
+
+        # Calculate the mean value of node
+        mean_y = np.mean(y)
+
+        # Base case for recursion: If any of the regulation criteria are met, return a leaf node
+        is_mse_zero = (self.criterion == 'mse' and self.MSE(y) == 0)
+        is_mae_zero = (self.criterion == 'mae' and self.MAE(y) == 0)
+
+        if (
+            curr_depth > self.max_depth or
+            len(X) < self.min_split_samples or
+            is_mse_zero or is_mae_zero
+        ):
+            return Node(None, mean_y)
+        else:
+            # Recursive case: If not a leaf node, compute the best split
+            (best_left, best_right, best_split_point) = self.best_split(X, y)
+
+            # If no valid split point is found, return a leaf node
+            if best_split_point is None:
+                return Node(None, mean_y)
+
+            # Check if the split leads to leaf nodes with too few samples
+            if (len(best_left) < self.min_leaf_samples or
+                    len(best_right) < self.min_leaf_samples):
+                return Node(None, mean_y)
+            else:
+                subtree = Node(best_split_point, mean_y)
+                X_left, y_left = best_left[:, :-1], best_left[:, -1]
+                X_right, y_right = best_right[:, :-1], best_right[:, -1]
+                subtree.left = self.__fit(
+                    subtree.left, curr_depth + 1, X_left, y_left)
+                subtree.right = self.__fit(
+                    subtree.right, curr_depth + 1, X_right, y_right)
+                return subtree
+
+    def fit(self, X, y):
+        '''Build the Regression Tree by recursively calling the __fit method.'''
         self.root = self.__fit(self.root, 1, X, y)
