2a3b78b9d3b714d1091f7d117aeca559e352f952
Xingsheng Xu <xuxingsheng163@gmail.com>
2023-07-19 14:26:23 -0700

    Complete Version 1.1

--- Linear Regression/Linear_Regression.py
+++ Linear Regression/Linear_Regression.py
@@ -3,14 +3,15 @@ import numpy as np
 from matplotlib.animation import FuncAnimation, PillowWriter
 
 
-class LinearRegressionBGD:
+class LinearRegression:
     """
-    Linear Regression using Batch Gradient Descent is a machine learning algorithm 
+    Linear Regression using either Stochastic Gradient Descent (SGD) or Batch Gradient Descent (BGD)
     where a linear model is trained to predict output values by iteratively adjusting 
-    its parameters based on the entire dataset to minimize the difference between predicted 
-    and actual output values.
+    its parameters based on the individual sample or the entire dataset to minimize the difference 
+    between predicted and actual output values.
 
     Args:
+        algorithm (str): 'BGD' for Batch Gradient Descent, 'SGD' for Stochastic Gradient Descent
         learning_rate (float, optional): Learning rate for Gradient Descent.
         max_iterations (int, optional): Maximum number of iterations for the Gradient Descent loop.
 
@@ -22,14 +23,16 @@ class LinearRegressionBGD:
         cost_memo (list): Memory of cost at each iteration.
     """
 
-    def __init__(self, learning_rate=0.1, max_iterations=100):
+    def __init__(self, algorithm='BGD', learning_rate=0.1, max_iterations=100):
+        self.algorithm = algorithm
         self.learning_rate = learning_rate
         self.max_iterations = max_iterations
         self.theta = np.zeros(2)
         self.theta_real = np.zeros(2)
         self.iteration = 0
-        self.cost = np.inf
+        self.cost = 0
         self.cost_memo = []
+        self.IsFitted = False
 
     def normalize(self, data):
         """Normalization Function"""
@@ -52,8 +55,12 @@ class LinearRegressionBGD:
     def predict_value(self, X):
         """Compute the predicted price using the real theta values"""
 
-        pred_Y = self.h_func_real(X)
-        return pred_Y
+        if not self.IsFitted:
+            raise ValueError(
+                "Model is not fitted, call 'fit' with appropriate arguments before using model.")
+        else:
+            pred_Y = self.h_func_real(X)
+            return pred_Y
 
     def initialize_plot(self, X, y):
         """Initialize plot for animation"""
@@ -62,13 +69,12 @@ class LinearRegressionBGD:
         self.ax.scatter(X, y, c='black', label='Target')
         self.ax.set_xlabel('Input X')
         self.ax.set_ylabel('Output y')
-        self.ax.set_title('Linear Regression (Batch Gradient Descent)')
         self.line, = self.ax.plot(
             X, self.h_func_real(X), 'r-', label='Predict Line')
         plt.legend()
 
-    def update(self, frame):
-        """Update function for the animation"""
+    def update_BGD(self, frame):
+        """Update function for BGD"""
 
         y_pred = self.h_func(self.normalized_X)
         error = self.normalized_y - y_pred
@@ -81,6 +87,39 @@ class LinearRegressionBGD:
 
         self.cost = np.sum(error ** 2) / (2 * self.X.shape[0])
         self.cost_memo.append(self.cost)
+        self.iteration += 1
+
+        self.line.set_ydata(self.h_func_real(self.X))
+        return self.line,
+
+    def update_SGD(self, frame):
+        """Update function for SGD"""
+
+        # Shuffle the data at each iteration
+        indices = np.arange(self.X.shape[0])
+        np.random.shuffle(indices)
+
+        self.cost = 0
+
+        for i in indices:
+            X_sample = self.normalized_X[i]
+            y_sample = self.normalized_y[i]
+
+            y_pred = self.h_func(X_sample)
+            error = y_sample - y_pred
+            grad = X_sample * error
+            self.theta += self.learning_rate * grad
+
+            # Update theta values based on the real-scale data
+            self.theta_real[1] = (self.y_dev / self.X_dev) * self.theta[1]
+            self.theta_real[0] = self.y_mean - self.theta_real[1] * self.X_mean
+
+            # Calculate and accumulate the cost for each sample
+            self.cost += error ** 2 / 2
+
+        # Append the total cost of the iteration to the memo
+        self.cost_memo.append(self.cost / self.X.shape[0])
+
         self.iteration += 1
 
         self.line.set_ydata(self.h_func_real(self.X))
@@ -100,6 +139,16 @@ class LinearRegressionBGD:
         # Initialize plot for animation
         self.initialize_plot(X, y)
 
+        if self.algorithm == 'BGD':
+            self.update = self.update_BGD
+            self.ax.set_title('Linear Regression (Batch Gradient Descent)')
+        elif self.algorithm == 'SGD':
+            self.update = self.update_SGD
+            self.ax.set_title(
+                'Linear Regression (Stochastic Gradient Descent)')
+        else:
+            raise ValueError('Invalid algorithm type. Choose "BGD" or "SGD".')
+
         # Animation settings
         self.animation = FuncAnimation(self.fig, self.update, frames=self.max_iterations,
                                        interval=50, blit=True, repeat=False)
@@ -107,7 +156,10 @@ class LinearRegressionBGD:
         # *Save animation as gif
         # writer = PillowWriter(fps=20)
         # self.animation.save(
-        #     './Linear Regression of Synthetic Data Set.gif', writer=writer)
+        #     f'./Linear Regression ({self.algorithm}) of Synthetic Data Set.gif', writer=writer)
 
         # Display the animation
         plt.show()
+
+        # The model has been fitted
+        self.IsFitted = True
