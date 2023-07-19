3a396ffecff8575eaf7b48e986f55b429365bae6
Xingsheng Xu <xuxingsheng163@gmail.com>
2023-07-19 16:39:24 -0700

    Complete Version 1.1

--- Linear Regression/Normal_Equations.py
+++ Linear Regression/Normal_Equations.py
@@ -1,51 +1,55 @@
-"""
-This program implements a linear regression model using Normal Equations to 
-predict house prices based on living area (in square feet).
-"""
-
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 
-# Load Data
-house_data = pd.read_csv('Linear Regression/dataset/house_price.csv')
 
-# Settings
-data_size = 100
+class NormalEquations:
+    """
+    LWLR is a non-parametric algorithm that fits the model to the data at prediction time, 
+    using a weight matrix to give higher importance to data points near the point of interest.
 
+    Args:
+        tau (float): The bandwidth parameter which determines the range of influence of the data points.
 
-def predict_price(sqft_living, theta_0, theta_1):
-    """Compute the predicted price using the real theta values"""
-    predicted_price = theta_0 + theta_1 * sqft_living
-    return predicted_price
+    Attributes:
+        X (np.array): The input training dataset.
+        y (np.array): The target values.
+    """
 
+    def __init__(self):
+        self.theta = np.zeros(2)
+        self.IsFitted = False
 
-# Training Set
-real_training_set_X = house_data['sqft_living'].head(data_size)
-real_training_set_Y = house_data['price'].head(data_size)
+    def fit(self, X, y):
+        """Train the model with the given training set."""
 
-# Design Matrix X as Inputs
-X = np.column_stack((np.ones_like(real_training_set_X), real_training_set_X))
+        self.X = np.column_stack((np.ones_like(X), X))
+        self.Y = y
 
-# Target Value Vector
-Y = real_training_set_Y
+        # Theta Found by Normal Functions to minimizes the mean squared error
+        # ! theta = (X.T*X)^-1*X.T*Y
+        self.theta = np.dot(np.dot(np.linalg.inv(
+            np.dot(self.X.T, self.X)), self.X.T), self.Y)
+        print(
+            f"Theta values: theta_0 = {self.theta[0]}, theta_1 = {self.theta[1]}")
+        self.IsFitted = True
 
-# Theta Found by Normal Functions to minimizes the mean squared error
-theta = np.dot(np.dot(np.linalg.inv(np.dot(X.T, X)), X.T),
-               Y)  # ! theta = (X.T*X)^-1*X.T*Y
-theta_0, theta_1 = theta
-print(f"Theta values: theta_0 = {theta_0}, theta_1 = {theta_1}")
+    def predict(self, X):
+        """Compute the predicted value."""
 
-# Predict price from real_training_set_X
-predicted_price = predict_price(real_training_set_X, theta_0, theta_1)
+        if not self.IsFitted:
+            raise ValueError(
+                "Model is not fitted, call 'fit' with appropriate arguments before using model.")
+        else:
+            pred_y = self.theta[0] + self.theta[1] * X
+            return pred_y
 
-# Plot the generated dataset
-plt.scatter(real_training_set_X, real_training_set_Y,
-            c='black', marker='o', label='Target')
-plt.plot(real_training_set_X, predicted_price,
-         'r', label='Normal Equations Prediction')
-plt.xlabel('Living Area (ft²)')
-plt.ylabel('House Price ($)')
-plt.title('Linear Regression (Normal Equations)')
-plt.legend()
-plt.show()
+    def plot(self, X, y, pred_y):
+        """Plot the generated dataset."""
+
+        plt.scatter(X, y, c='black', marker='o', label='Target')
+        plt.plot(X, pred_y, 'r', label='Normal Equations Prediction')
+        plt.xlabel('Input Data')
+        plt.ylabel('Predict Value')
+        plt.title('Linear Regression (Normal Equations)')
+        plt.legend()
+        plt.show()
