9158bfed2ace6cc48f7e9442cdc027c776e66745
Xingsheng Xu <xuxingsheng163@gmail.com>
2023-07-19 11:25:37 -0700

    Complete Version 1.0

--- Linear Regression/LinearRegression_BGD.py
+++ Linear Regression/LinearRegression_BGD.py
@@ -1,124 +1,113 @@
-"""
-Batch Gradient Descent (BGD) for predicting house prices based on living area.
-
-This code implements a linear regression model using Batch Gradient Descent (BGD) 
-to predict the price of a house based on its living area (sqft_living). The model is trained 
-on a house price dataset loaded from a CSV file, and the real-scale theta values are computed 
-after normalizing the data. The training process is animated using `matplotlib`'s FuncAnimation 
-class, and the cost vs. iteration curve is plotted after training. The prediction for the price 
-of a house with 2000 sqft of living space is also computed as an example.
-"""
-
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 from matplotlib.animation import FuncAnimation, PillowWriter
 
-# Load Data
-house_data = pd.read_csv('Linear Regression/dataset/house_price.csv')
 
-# Settings
-theta = np.zeros(2)
-theta_0_real, theta_1_real = np.zeros(2)
-data_size = 100
-iteration = 0
-cost = np.inf
-learning_rate = 0.1
-max_iterations = 100
-cost_memo = []
+class LinearRegressionBGD:
+    """
+    Linear Regression using Batch Gradient Descent is a machine learning algorithm 
+    where a linear model is trained to predict output values by iteratively adjusting 
+    its parameters based on the entire dataset to minimize the difference between predicted 
+    and actual output values.
 
+    Args:
+        learning_rate (float, optional): Learning rate for Gradient Descent.
+        max_iterations (int, optional): Maximum number of iterations for the Gradient Descent loop.
 
-def normalize(data):
-    """Normalization Function"""
-    mean = np.mean(data, axis=0)
-    std_dev = np.std(data, axis=0)
-    normalized_data = (data - mean) / std_dev
-    return normalized_data, mean, std_dev
+    Attributes:
+        theta (np.array): Model weights.
+        theta_real (np.array): Real-scale model weights.
+        iteration (int): Counter for iterations.
+        cost (float): Current cost value.
+        cost_memo (list): Memory of cost at each iteration.
+    """
 
+    def __init__(self, learning_rate=0.1, max_iterations=100):
+        self.learning_rate = learning_rate
+        self.max_iterations = max_iterations
+        self.theta = np.zeros(2)
+        self.theta_real = np.zeros(2)
+        self.iteration = 0
+        self.cost = np.inf
+        self.cost_memo = []
 
-def h_func(theta, x):
-    """Hypothesis Function"""
-    return theta[0] + theta[1] * x
+    def normalize(self, data):
+        """Normalization Function"""
 
+        mean = np.mean(data, axis=0)
+        std_dev = np.std(data, axis=0)
+        normalized_data = (data - mean) / std_dev
+        return normalized_data, mean, std_dev
 
-def h_func_real(theta_0, theta_1, x):
-    """Hypothesis Function for real-scale data"""
-    return theta_0 + theta_1 * x
+    def h_func(self, X):
+        """Hypothesis Function"""
 
+        return np.dot(X, self.theta)
 
-def predict_price(sqft_living, theta_0_real, theta_1_real):
-    """Compute the predicted price using the real theta values"""
-    predicted_price = h_func_real(theta_0_real, theta_1_real, sqft_living)
-    return predicted_price
+    def h_func_real(self, X):
+        """Hypothesis Function for real-scale data"""
 
+        return self.theta_real[0] + self.theta_real[1] * X
 
-# Training Set
-real_training_set_X = house_data['sqft_living'].head(data_size)
-real_training_set_Y = house_data['price'].head(data_size)
-training_set_X, X_mean, X_dev = normalize(
-    house_data['sqft_living'].head(data_size))
-training_set_Y, Y_mean, Y_dev = normalize(house_data['price'].head(data_size))
+    def predict_value(self, X):
+        """Compute the predicted price using the real theta values"""
 
-# Add a column of ones to the training set for theta_0
-X = np.vstack((np.ones_like(training_set_X), training_set_X)).T
+        pred_Y = self.h_func_real(X)
+        return pred_Y
 
-# Initialize the plot
-fig, ax = plt.subplots()
+    def initialize_plot(self, X, y):
+        """Initialize plot for animation"""
 
-ax.scatter(real_training_set_X, real_training_set_Y, c='black', label='Target')
-ax.set_xlabel('Living Area (ft²)')
-ax.set_ylabel('House Price ($)')
-ax.set_title('Linear Regression (Batch Gradient Descent)')
-line, = ax.plot(real_training_set_X, h_func_real(theta_0_real, theta_1_real, real_training_set_X),
-                'r-', label='Predict Line')
-plt.legend()
+        self.fig, self.ax = plt.subplots()
+        self.ax.scatter(X, y, c='black', label='Target')
+        self.ax.set_xlabel('Input X')
+        self.ax.set_ylabel('Output y')
+        self.ax.set_title('Linear Regression (Batch Gradient Descent)')
+        self.line, = self.ax.plot(
+            X, self.h_func_real(X), 'r-', label='Predict Line')
+        plt.legend()
 
+    def update(self, frame):
+        """Update function for the animation"""
 
-def update(frame):
-    """Update function for the animation"""
-    global theta, cost, iteration, theta_0_real, theta_1_real
-    y_pred = h_func(theta, training_set_X)
-    error = training_set_Y - y_pred
-    grad = np.dot(X.T, error)
-    theta += learning_rate * grad / data_size
+        y_pred = self.h_func(self.normalized_X)
+        error = self.normalized_y - y_pred
+        grad = np.dot(self.normalized_X.T, error)
+        self.theta += self.learning_rate * grad / self.X.shape[0]
 
-    # Update theta values based on the real-scale data
-    theta_1_real = (Y_dev / X_dev) * theta[1]
-    theta_0_real = Y_mean - theta_1_real * X_mean
+        # Update theta values based on the real-scale data
+        self.theta_real[1] = (self.y_dev / self.X_dev) * self.theta[1]
+        self.theta_real[0] = self.y_mean - self.theta_real[1] * self.X_mean
 
-    cost = np.sum(error ** 2) / (2 * data_size)
-    cost_memo.append(cost)
-    iteration += 1
+        self.cost = np.sum(error ** 2) / (2 * self.X.shape[0])
+        self.cost_memo.append(self.cost)
+        self.iteration += 1
 
-    line.set_ydata(h_func_real(
-        theta_0_real, theta_1_real, real_training_set_X))
-    return line,
+        self.line.set_ydata(self.h_func_real(self.X))
+        return self.line,
 
+    def fit(self, X, y):
+        """Train the model with the given training set"""
 
-# Animation settings
-ani = FuncAnimation(fig, update, frames=max_iterations,
-                    interval=50, blit=True, repeat=False)
+        self.X = X
+        self.normalized_X, self.X_mean, self.X_dev = self.normalize(X)
+        self.normalized_y, self.y_mean, self.y_dev = self.normalize(y)
 
-# Save the animation as a GIF
-# writer = PillowWriter(fps=20)
-# ani.save('Linear_Regression/Linear Regression of House Price.gif', writer=writer)
+        # Add a column of ones to the training set for theta_0
+        self.normalized_X = np.vstack(
+            (np.ones_like(self.normalized_X), self.normalized_X)).T
 
-# Display the animation
-plt.show()
+        # Initialize plot for animation
+        self.initialize_plot(X, y)
 
-print(f"Training finished after {iteration} iterations.")
-print(f"Theta values: theta_0 = {theta_0_real}, theta_1 = {theta_1_real}")
+        # Animation settings
+        self.animation = FuncAnimation(self.fig, self.update, frames=self.max_iterations,
+                                       interval=50, blit=True, repeat=False)
 
-# Plot cost vs. iteration
-fig2, ax2 = plt.subplots()
-ax2.plot(range(1, iteration + 1), cost_memo)
-ax2.set_xlabel('Iteration')
-ax2.set_ylabel('Cost')
-ax2.set_title('Cost vs. Iteration')
-plt.show()
+        # *Save animation as gif
+        # writer = PillowWriter(fps=20)
+        # self.animation.save(
+        #     './Linear Regression of Synthetic Data Set.gif', writer=writer)
 
-# Example usage: Predict the price of a house with 2000 sqft of living space
-sqft_living = 2000
-predicted_price = predict_price(sqft_living, theta_0_real, theta_1_real)
-print(
-    f"The predicted price for a house with {sqft_living} sqft of living space is: ${predicted_price:.2f}")
+        # Display the animation
+        plt.show()
---
+++ Linear Regression/Linear_Regression_utils.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.animation import PillowWriter
+from sklearn.metrics import r2_score
+
+
+def evaluate_model(model, test_y, pred_y):
+    """Evaluates the performance of a model and prints the R-squared score."""
+
+    # Calculate R-squared
+    r2 = r2_score(test_y, pred_y)
+
+    print(f"Training finished after {model.iteration} iterations.")
+    print(
+        f"Theta values: theta_0 = {model.theta_real[0]}, theta_1 = {model.theta_real[1]}")
+    print("R-squared score (R²): ", r2)
+
+
+def plot_cost_vs_iteration(model):
+    """Plot cost vs. iteration"""
+
+    plt.figure()
+    plt.plot(range(1, model.iteration + 1), model.cost_memo)
+    plt.xlabel('Iteration')
+    plt.ylabel('Cost')
+    plt.title('Cost vs. Iteration')
+    plt.show()
